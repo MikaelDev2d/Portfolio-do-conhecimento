d51a335d7d64e004dd1507982cb7deb9b02b7450
Mikael Oliveira <Mikaelroblox076@gmail.com>
2025-08-08 19:28:34 -0300

    Update Nome&Idade.py

--- Python/Códigos básicos de lógica/Nome&Idade.py
+++ Python/Códigos básicos de lógica/Nome&Idade.py
@@ -1,1 +1,16 @@
+apresentacao = input("Olá tudo bem? Você pode se apresentar para nós? (s/n):  ")
+if apresentacao == s:
+  print("Certo! vamos lá!")
+  nome = input("Qual é o seu nome aventureiro?:  ")
+  print(f"Uau {nome} é um nome bem chique!")
+  idade = input(int("Qual é a sua idade?:  ")
+  if idade >= 18:
+                print(f"Vejo que você é de maior {nome}!")
+  else:
+                print(f"Vejo que você é de menor {nome}!")
+else:
+  print("Tudo bem! até a próxima vez!  ")
+  print("Diálogo encerrado...  ")
+  exit()
+
 
