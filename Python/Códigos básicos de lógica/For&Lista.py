07051ecaba7bc34daf54d7c314e21582632aa490
Mikael Oliveira <Mikaelroblox076@gmail.com>
2025-08-09 16:44:49 -0300

    Create For&Lista.py

---
+++ Python/Códigos básicos de lógica/For&Lista.py
@@ -0,0 +1,7 @@
+listacompras = ["laranja, maçã, tomate, pão"]
+for frutas in listacompras:
+    print(frutas)
+
+listaprecos = [1000, 3300, 1244, 9300]
+for precos in listaprecos:
+    print(precos)
